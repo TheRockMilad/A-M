b3d30a8340d6c4f585d1ed20c5e2ca7809e8cf42
TheRockMilad <therock.milad2@outlook.com>
2024-07-13 12:17:02 +0330

    add route article

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 require('dotenv').config()
+const ArticleRouter = require("./src/routes/article/routes")
+
+app.use(express.json())
+app.use(express.urlencoded({extended : ture}))
+
+app.use("/api/courses",ArticleRouter)
 
 const port = process.env.PORT || 4000
 app.listen(port,()=>{
