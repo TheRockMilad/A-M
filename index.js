c9826d2796c44ed61c00001f148e39b083bf5e26
TheRockMilad <therock.milad2@outlook.com>
2024-07-13 12:21:48 +0330

    add auth route

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 require('dotenv').config()
 const ArticleRouter = require("./src/routes/article/routes")
+const AuthRouter = require("./src/routes/auth/routes")
 
 app.use(express.json())
 app.use(express.urlencoded({extended : ture}))
 
 app.use("/api/courses",ArticleRouter)
+app.use("/api/auth",authRouter)
 
 const port = process.env.PORT || 4000
 app.listen(port,()=>{
